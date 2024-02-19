10f1b7ca21fade5641b7c8040f1543878b4bbe1a
Ömer Faruk Yanık <omerfarukyanik212313@gmail.com>
2024-02-19 18:40:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+int month = DateTime.Now.Month;
+
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak ayındasınız!");
+        break;
+    case 2:
+        Console.WriteLine("Şubat ayındasınız!");
+        break;
+    case 4:
+        Console.WriteLine("Nisan ayındasınız!");
+        break;
+    case 3:
+        Console.WriteLine("Mart ayındasınız!");
+        break;
+    default:
+        Console.WriteLine("Yanlış veri girdiniz!");
+        break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış ayıdnasınız!");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkbahar ayındasınız!");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz ayındasınız!");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonbahar ayındasınız!");
+        break;
+    default:
+        break;
+}
